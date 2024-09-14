2678899ed9bfcf04e472270dbd7e545d189fa227
osama aslam <osamaaslamm029@gmail.com>
2024-09-14 16:19:43 +0500

    sample

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
 app.get("/login", (req, res) => {
   res.send("<h1>Please Login First</h1>");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
